27f781f8f0d16c90675d6b29397c4ff9c325199c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-11-22 22:05:31 -0600

    counter app

---
+++ src/components/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count, setCount] = useState(100)
+
+    const handleIncrement = () => {
+        setCount(count + 1)
+    }
+
+    const handleDecrement = () => {
+        setCount(count - 1)
+    }
+
+    return (
+        <div>
+            <button onClick={handleIncrement}>+</button>
+            <span>{count}</span>
+            <button onClick={handleDecrement}>-</button>
+        </div>
+    )
+}
